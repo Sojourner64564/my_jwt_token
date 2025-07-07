52cd6fbeba3e58c3480367c86109f30ac053be95
Лыкасов Александр <qwerty2347136@gmail.com>
2025-07-08 00:09:50 +0500

    e,hfyfубрана лишная зависимость

--- lib/feature/main_screen_feature/presentation/main_screen.dart
+++ lib/feature/main_screen_feature/presentation/main_screen.dart
@@ -1,12 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:my_jwt_token/core/injectable/injectable.dart';
-import 'package:my_jwt_token/feature/common_feature/data/data_source/local_ds/security_storage_client.dart';
 import 'package:my_jwt_token/feature/main_screen_feature/presentation/main_page.dart';
 
 class MainScreen extends StatelessWidget {
-   MainScreen({super.key});
-
-   final securityStorage = getIt<SecurityStorageClient>();
+   const MainScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
